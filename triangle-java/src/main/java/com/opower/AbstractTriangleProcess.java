2f7ec3e6e16b9f5ff15027f39189dbc4d2a93a05
tom.vaughan <tom_vaughan@hotmail.com>
2012-10-17 16:29:41 -0400

    Renaming variable "i" to be more descriptive "triangleIndex"

--- triangle-java/src/main/java/com/opower/AbstractTriangleProcess.java
+++ triangle-java/src/main/java/com/opower/AbstractTriangleProcess.java
@@ -14,19 +14,20 @@ public class AbstractTriangleProcess {
     protected final static Integer SEARCH_LIMIT = 50000;
     
     protected static int BIGGEST_FACTORS_SO_FAR = 0;
-    protected static void printUpdate(StopWatch stopWatch, int i, Long triangleNumber, Integer[] factors) {
+    protected static void printUpdate(StopWatch stopWatch, int triangleIndex, Long triangleNumber, Integer[] factors) {
         if(factors.length > BIGGEST_FACTORS_SO_FAR) {
             stopWatch.suspend();
-            System.out.print(String.format("%10d is triangle number #%6d with %4d factors: ", triangleNumber, i, factors.length));
+            System.out.print(String.format("%10d is triangle number #%6d with %4d factors: ", 
+                    triangleNumber, triangleIndex, factors.length));
             System.out.println(Factors.printFactors(triangleNumber, factors));
             BIGGEST_FACTORS_SO_FAR = factors.length;
             stopWatch.resume();    
         }
     }
 
-    protected static void printSolution(Long triangleNumber, int i, int length, String time) {
+    protected static void printSolution(Long triangleNumber, int triangleIndex, int length, String time) {
         System.out.println("");
-        System.out.println(triangleNumber + " is the first triangle number (the " + i + "th) "
+        System.out.println(triangleNumber + " is the first triangle number (the " + triangleIndex + "th) "
                 + "to have at least " + FACTOR_LIMIT + " factors (it has " + length + " factors)");
         System.out.println("Finding this solution took " + time);
     }
